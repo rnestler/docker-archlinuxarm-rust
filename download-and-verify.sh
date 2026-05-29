247bb7d35c40c6289b6ba76d56dacbdd149135d6
Raphael Nestler <raphael.nestler@gmail.com>
2026-05-29 11:28:22 +0200

    Verify directly with the given public key
    For this to work the public key needs to be in the gpg native format. I
    exported it again with:
    
        gpg --export 68B3537F39A313B3E574D06777193F152BDBE6A6 > archlinuxarm.gpg

--- download-and-verify.sh
+++ download-and-verify.sh
@@ -5,5 +5,4 @@ set -euo pipefail
 curl -L "http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz" -o ArchLinuxARM-aarch64-latest.tar.gz
 curl -L "http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz.sig" -o ArchLinuxARM-aarch64-latest.tar.gz.sig
 
-gpg --import archlinuxarm.gpg
-gpg --verify ArchLinuxARM-aarch64-latest.tar.gz.sig ArchLinuxARM-aarch64-latest.tar.gz
+gpg --no-default-keyring --keyring ./archlinuxarm.gpg --verify ArchLinuxARM-aarch64-latest.tar.gz.sig ArchLinuxARM-aarch64-latest.tar.gz
