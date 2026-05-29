2b244f31ef6dd9a49c55fb38516e97f404261c27
Raphael Nestler <raphael.nestler@gmail.com>
2026-05-29 11:14:16 +0200

    Use gpg to verify archive integrity

--- download-and-verify.sh
+++ download-and-verify.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-curl -L "http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz.md5" -o ArchLinuxARM-aarch64-latest.tar.gz.md5
 curl -L "http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz" -o ArchLinuxARM-aarch64-latest.tar.gz
+curl -L "http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz.sig" -o ArchLinuxARM-aarch64-latest.tar.gz.sig
 
-git diff --exit-code ArchLinuxARM-aarch64-latest.tar.gz.md5 || { echo "md5 changed. Probably the rootfs just got updated. Make sure it is authentic before committing"; exit 1; }
-
-md5sum -c ArchLinuxARM-aarch64-latest.tar.gz.md5
+gpg --import archlinuxarm.gpg
+gpg --verify ArchLinuxARM-aarch64-latest.tar.gz.sig ArchLinuxARM-aarch64-latest.tar.gz
